becfa9bd95e6f58ed72e63286f26193097383254
Ryan <rbrickproductions123@gmail.com>
2014-05-13 15:43:13 -0500

    Problem 1 in c#

---
+++ problem-1-csharp.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Test
+{
+	public static void Main()
+    {
+		
+		int sum = 0;
+		
+		for(int i =1; i < 1000; i++) {
+			if(i%3 == 0 || i%5 == 0) {
+				sum += i;
+			}
+		}
+		Console.WriteLine(sum);
+	}
+}
